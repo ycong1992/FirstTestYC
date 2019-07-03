7aca73b8caf4e613ce43020132f7925a2a83d91e
xieyuecong <xieyuecong@0easy.com>
2019-07-03 09:11:32 +0800

    Initial Commit

---
+++ FirstTestYC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstTestYC
+//
+//  Created by SZOeasy on 2019/7/3.
+//  Copyright © 2019 ycong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstTestYC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FirstTestYC
+//
+//  Created by SZOeasy on 2019/7/3.
+//  Copyright © 2019 ycong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
