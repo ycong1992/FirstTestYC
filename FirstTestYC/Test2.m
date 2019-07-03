9caa3e1a924d4f944e2703c6e58c31c814fde789
xieyuecong <xieyuecong@0easy.com>
2019-07-03 10:07:36 +0800

    添加新文件

---
+++ FirstTestYC/Test2.m
@@ -0,0 +1,13 @@
+//
+//  Test2.m
+//  FirstTestYC
+//
+//  Created by SZOeasy on 2019/7/3.
+//  Copyright © 2019 ycong. All rights reserved.
+//
+
+#import "Test2.h"
+
+@implementation Test2
+
+@end
