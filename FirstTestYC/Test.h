4ccd17a61e042114a61aa684cba71577f6fe7cb2
xieyuecong <xieyuecong@0easy.com>
2019-07-03 09:15:35 +0800

    22

---
+++ FirstTestYC/Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  FirstTestYC
+//
+//  Created by SZOeasy on 2019/7/3.
+//  Copyright © 2019 ycong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FirstTestYC/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  FirstTestYC
+//
+//  Created by SZOeasy on 2019/7/3.
+//  Copyright © 2019 ycong. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
